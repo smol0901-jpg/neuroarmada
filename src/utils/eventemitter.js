b8185b85d0530f1764b3a76643809e3029daab95
smol0901-jpg <smol0901@gmail.com>
2026-06-01 17:59:56 +0300

    feat: добавить eventemitter

---
+++ src/utils/eventemitter.js
@@ -0,0 +1,35 @@
+/**
+ * EventEmitter - Простая система событий
+ */
+
+export class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+    return () => this.off(event, callback);
+  }
+
+  off(event, callback) {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event].filter(cb => cb !== callback);
+  }
+
+  emit(event, ...args) {
+    if (!this.events[event]) return;
+    this.events[event].forEach(cb => cb(...args));
+  }
+
+  once(event, callback) {
+    const wrapper = (...args) => {
+      this.off(event, wrapper);
+      callback(...args);
+    };
+    this.on(event, wrapper);
+  }
+}
